dc11a651c83fa4379c4bf9c3e00e97df005964b6
Maricar Walters <walterskai7@gmail.com>
2021-03-10 10:38:44 -0600

    Added Angular file, added components, model, service, and class files

---
+++ PaymentApp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { PaymentDetailsComponent } from './payment-details/payment-details.component';
+import { PaymentDetailFormComponent } from './payment-details/payment-detail-form/payment-detail-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PaymentDetailsComponent,
+    PaymentDetailFormComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ PaymentApp/src/app/shared/payment-detail.model.ts
@@ -0,0 +1,7 @@
+export class PaymentDetail {
+    paymentDetaildId: number;
+    cardOwnerName: string;
+    cardNumber: string;
+    expirationDate: string;
+    securityCode: string;
+}
