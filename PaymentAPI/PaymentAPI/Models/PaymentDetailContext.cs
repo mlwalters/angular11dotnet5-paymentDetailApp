dba98c07dbd5df082ef8163686e21ad11421ad42
Maricar Walters <walterskai7@gmail.com>
2021-03-10 08:03:19 -0600

    Initial commit for PaymentDetail App

---
+++ PaymentAPI/PaymentAPI/Models/PaymentDetailContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetailContext : DbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options) : base(options) // ctor Tab Tab to make a constructor
+        {
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+
+    }
+}
